1222cd2c8a73d4ef831429bdecd77574f50f8e68
Javier Cordero <jcorderomartinez@gmail.com>
2014-02-22 19:46:13 +0100

    Adding travis setup

--- runtests.py
+++ runtests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import django
 from django.conf import settings
 
 import gevent.monkey
@@ -8,24 +9,46 @@ gevent.monkey.patch_all()
 import psycogreen.gevent
 psycogreen.gevent.patch_psycopg()
 
-
-settings.configure(
-    DEBUG=True,
-    DATABASES={
-        'default': {
-            'ENGINE': 'django_db_geventpool.backends.postgresql_psycopg2',
-            'NAME': 'test',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'OPTIONS': {'autocommit': True},
-        }
-    },
-    INSTALLED_APPS=(
-        'django_db_geventpool',
-        'tests',
-    ),
-    USE_TZ=True,
-)
+if django.get_version().startswith('1.5'):
+    settings.configure(
+        DEBUG=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django_db_geventpool.backends.postgresql_psycopg2',
+                'NAME': 'test',
+                'USER': 'postgres',
+                'PASSWORD': 'postgres',
+                'OPTIONS': {'autocommit': True},
+            }
+        },
+        INSTALLED_APPS=(
+            'django_db_geventpool',
+            'tests',
+        ),
+        USE_TZ=True,
+    )
+else:
+    settings.configure(
+        DEBUG=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django_db_geventpool.backends.postgresql_psycopg2',
+                'NAME': 'test',
+                'USER': 'postgres',
+                'PASSWORD': 'postgres',
+                'ATOMIC_REQUESTS': False,
+                'CONN_MAX_AGE': 0,
+                'OPTIONS': {
+                    'autocommit': True,
+                    },
+            }
+        },
+        INSTALLED_APPS=(
+            'django_db_geventpool',
+            'tests',
+        ),
+        USE_TZ=True,
+    )
 
 from django.test.simple import DjangoTestSuiteRunner
 test_runner = DjangoTestSuiteRunner(verbosity=1)
