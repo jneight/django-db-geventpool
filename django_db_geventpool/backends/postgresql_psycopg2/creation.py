293b1d492cfd3c5542c78acffbbdebf4933b6d85
Ask Holme <ask@askholme.dk>
2014-08-24 14:14:20 +0200

    Handle open connections when creating the test database

--- django_db_geventpool/backends/postgresql_psycopg2/creation.py
+++ django_db_geventpool/backends/postgresql_psycopg2/creation.py
@@ -7,4 +7,6 @@ class DatabaseCreation(OriginalDatabaseCreation):
     def _destroy_test_db(self, test_database_name, verbosity):
         self.connection.closeall()
         return super(DatabaseCreation, self)._destroy_test_db(test_database_name, verbosity)
-
+    def _create_test_db(self, verbosity, autoclobber):
+        self.connection.closeall()
+        return super(DatabaseCreation, self)._create_test_db(verbosity, autoclobber)
