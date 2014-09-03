7f4c1bb9254b56c9aa595d30cf51e7ccf0279413
Javier Cordero <jcorderomartinez@gmail.com>
2014-09-03 11:51:42 +0200

    version 1.15

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-db-geventpool',
-    version='1.10',
+    version='1.15',
     install_requires=[
         'gevent>=1.0',
         'django>=1.5',
