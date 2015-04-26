15eb25887b958f04110adb6c8a757e522dece147
Javier Cordero <jcorderomartinez@gmail.com>
2015-04-26 19:10:21 +0200

    Support for tests in django 1.8

--- django_db_geventpool/backends/postgresql_psycopg2/creation.py
+++ django_db_geventpool/backends/postgresql_psycopg2/creation.py
@@ -25,7 +25,20 @@ class DatabaseCreationMixin17(object):
         return super(DatabaseCreationMixin17, self)._destroy_test_db(test_database_name, verbosity)
 
 
-if django.VERSION >= (1, 7):
+class DatabaseCreationMixin18(object):
+    def _create_test_db(self, verbosity, autoclobber, keepdb=False):
+        self.connection.closeall()
+        return super(DatabaseCreationMixin18, self)._create_test_db(verbosity, autoclobber, keepdb)
+
+    def _destroy_test_db(self, test_database_name, verbosity):
+        self.connection.closeall()
+        return super(DatabaseCreationMixin18, self)._destroy_test_db(test_database_name, verbosity)
+
+
+if django.VERSION >= (1, 8):
+    class DatabaseCreationMixin(DatabaseCreationMixin18):
+        pass
+elif django.VERSION >= (1, 7):
     class DatabaseCreationMixin(DatabaseCreationMixin17):
         pass
 else:
