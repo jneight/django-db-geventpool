0150803fab3c9161a46f855b6ed39cc7c7bb448b
Javier Cordero <javier@aplazame.com>
2020-03-02 12:33:56 +0100

    fix #40 remove python2 code from tests

--- tests/models.py
+++ tests/models.py
@@ -1,10 +1,8 @@
 # coding=utf-8
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class TestModel(models.Model):
     data = models.CharField(max_length=32, blank=True)
 
