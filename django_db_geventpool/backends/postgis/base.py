e8eb90a5c8f76f538751cdd274ef1e572c8809b2
Florian Demmer <fdemmer@gmail.com>
2024-07-07 17:50:59 +0200

    Fix PostGIS backend (#85)
    * Add test runner for postgis backend
    
    * Fix DatabaseWrapper for postgis
    
    * Add docker-compose with postgis and tox file to run pg2/3 and gis tests
    
    * Update Django classifiers

--- django_db_geventpool/backends/postgis/base.py
+++ django_db_geventpool/backends/postgis/base.py
@@ -3,12 +3,17 @@ from django.contrib.gis.db.backends.postgis.base import (
 )
 
 try:
+    # try psycopg3
     import psycopg  # noqa
-    from ..postgresql_psycopg3.base import DatabaseWrapperMixin
+    INTRANS = psycopg.pq.TransactionStatus.INTRANS
+    from ..postgresql_psycopg3.base import base, PostgresConnectionPool
 except ImportError:
-    # fallback to psycopg3
-    from ..postgresql_psycopg2.base import DatabaseWrapperMixin
+    # fallback to psycopg2
+    import psycopg2
+    INTRANS = psycopg2.extensions.TRANSACTION_STATUS_INTRANS
+    from ..postgresql_psycopg2.base import base, PostgresConnectionPool
 
 
-class DatabaseWrapper(DatabaseWrapperMixin, OriginalDatabaseWrapper):
-    pass
+class DatabaseWrapper(base.DatabaseWrapperMixin, OriginalDatabaseWrapper):
+    pool_class = PostgresConnectionPool
+    INTRANS = INTRANS
